ea7fc6c346aa9ab81bc913552285ba4056665c47
Maicon Borba <maiconbsconceicao@gmail.com>
2024-01-21 23:27:27 -0300

    updated cors header

--- Backend/server.js
+++ Backend/server.js
@@ -8,6 +8,7 @@ const app = express()
 app.use(express.json())
 app.use(cors({
     origin: ['http://localhost:5000'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Custom-Header'],
     methods: ['POST', 'GET', 'DELETE', 'PUT'],
     credentials: true
 }))
