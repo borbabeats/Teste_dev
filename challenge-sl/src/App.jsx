6d0e3a4e7a9fb75238812e7c7af13a26c559c5fe
Maicon Borba <maiconbsconceicao@gmail.com>
2024-01-26 20:31:14 -0300

    Protecao de rotas

--- challenge-sl/src/App.jsx
+++ challenge-sl/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import PessoasDashboard from './Pages/PessoasDashboard.jsx'
 import Login from './Pages/Login.jsx'
 import ProtocolosDashboard from './Pages/ProtocolosDashboard.jsx'
@@ -7,26 +7,31 @@ import CadastraPessoa from './Pages/Pessoa.jsx'
 import CadastraProtocolo from './Pages/Protocolo.jsx'
 import Home from './Pages/Home.jsx'
 import Register from './Pages/Register.jsx'
-
-
+import { useCookies } from "react-cookie"
 
 
 
 function App() {
+
+  const [cookies, setCookie] = useCookies(['token'])
+
+  const isAuthenticated = !!cookies.token
+
   return (
+
     <div className='App'>
     <BrowserRouter>
       <Routes>
 
         
         <Route path='/login' element={<Login/>}/>
-        
-          <Route path='/register' element={<Register/>}/>
-          <Route path='/'  element={<Home/>}/>
-          <Route path='/dashboardpessoas' element={<PessoasDashboard/>}/>
-          <Route path='/dashboardprotocolo' element={<ProtocolosDashboard/>}/>
-          <Route path='/cadastrapessoa' element={<CadastraPessoa/>}/>
-          <Route path='/cadastraprotocolo' element={<CadastraProtocolo/>}/>
+        <Route path='/register' element={<Register/>}/>
+          
+          <Route path='/'  element={isAuthenticated ? <Home/> : <Navigate to='/login'/>} />
+          <Route path='/dashboardpessoas' element={isAuthenticated ? <PessoasDashboard/> : <Navigate to='/login'/>}/>
+          <Route path='/dashboardprotocolo' element={isAuthenticated ? <ProtocolosDashboard/> : <Navigate to='/login' />}/>
+          <Route path='/cadastrapessoa' element={isAuthenticated ? <CadastraPessoa/> : <Navigate to='/login'/>}/>
+          <Route path='/cadastraprotocolo' element={isAuthenticated ? <CadastraProtocolo/> : <Navigate to='/login'/>}/>
     
       </Routes>
     </BrowserRouter>
--- challenge-sl/src/Utils/validation.js
+++
@@ -1,5 +0,0 @@
-const validarSenha = (senha) => {
-    return senha?.toString().length > 6
-}
-
-export {validarSenha}
