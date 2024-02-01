26779dafa668eb20d34b3ba5f88d38b9c56c1ce8
Maicon Borba <maiconbsconceicao@gmail.com>
2024-01-31 21:17:16 -0300

    Api communication is ok

--- challenge-sl/src/Pages/Login.jsx
+++ challenge-sl/src/Pages/Login.jsx
@@ -6,6 +6,7 @@ import { useNavigate } from 'react-router-dom';
 
 
 
+
 const Login = () => {
    const [values, setValues] = useState([])  
  
--- challenge-sl/src/Services/Api.js
+++ challenge-sl/src/Services/Api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = 'http://localhost:5000';
+const BASE_URL = 'https://teste-dev-server-side.onrender.com';
 const api = axios.create({
     baseURL: BASE_URL
 });
