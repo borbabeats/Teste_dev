f3827de0b3305a9305066015d2bb8ff63caab25c
Maicon Borba <maiconbsconceicao@gmail.com>
2024-02-05 18:14:30 -0300

    Adicionando configuracoes ao CORS e retirando body-parser

--- Backend/server.js
+++ Backend/server.js
@@ -1,16 +1,18 @@
 const express = require('express')
 const cors = require('cors')
 const router = require('./src/routes/routes.js')
-const bodyParser = require('body-parser')
+
 const app = express()
 app.use(express.json())
 app.use(cors({
-    origin: ['https://crud-example-d15v.onrender.com'],
+    origin: 'https://crud-example-d15v.onrender.com',
     methods: ['POST', 'GET', 'DELETE', 'PUT'],
+    allowHeaders:'*',
+    maxAge: 86400,
     credentials: true
 }))
+
 app.use(router)
-app.use(bodyParser.json())
 app.listen(5000, () => {
     console.log('Aplicacao rodando na porta 5000')
 })
