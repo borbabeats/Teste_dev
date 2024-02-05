295c37e4c3a6b6e3c1871568ed0d9a72b13cb9b0
Maicon Borba <maiconbsconceicao@gmail.com>
2024-02-05 19:14:07 -0300

    Server config

--- Backend/server.js
+++ Backend/server.js
@@ -2,16 +2,13 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 
-
 const router = require('./src/routes/routes.js')
 app.use(router)
-
-
 
 app.use(cors({
     origin: 'https://crud-example-d15v.onrender.com',
     methods: ['POST', 'GET', 'DELETE', 'PUT'],
-    allowHeaders:'*',
+    allowedHeaders:'*',
     maxAge: 86400,
     credentials: true
 }))
