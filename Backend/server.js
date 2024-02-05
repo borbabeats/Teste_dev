90718b7d479954b0ff4648fa2dda2d6064e29f3c
Maicon Borba <maiconbsconceicao@gmail.com>
2024-02-04 23:40:11 -0300

    Changing frontend deployement

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 const app = express()
 app.use(express.json())
 app.use(cors({
-    origin: ['https://teste-dev-client-side.onrender.com'],
+    origin: ['https://crud-example-d15v.onrender.com'],
     methods: ['POST', 'GET', 'DELETE', 'PUT'],
     credentials: true
 }))
