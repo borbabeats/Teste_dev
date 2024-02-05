f63da225da23df659f96cf9416d55549cac2404b
Maicon Borba <maiconbsconceicao@gmail.com>
2024-02-04 22:49:40 -0300

    Migring comeback to vercel

--- challenge-sl/src/Services/Api.js
+++ challenge-sl/src/Services/Api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = 'https://teste-dev-server-side.onrender.com';
+const BASE_URL = 'https://teste-dev-weld.vercel.app';
 const api = axios.create({
     baseURL: BASE_URL
 });
