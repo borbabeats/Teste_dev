cb21d4d1fc7f2cb1f395375eac339935f317a9bf
Maicon Borba <maiconbsconceicao@gmail.com>
2024-02-06 18:57:19 -0300

    Changing listen port in server to 8080

--- Backend/server.js
+++ Backend/server.js
@@ -1,22 +1,23 @@
 const express = require('express')
 const cors = require('cors')
+const app = express()
 
 
 const router = require('./src/routes/routes.js')
 app.use(router)
 
-const app = express()
+
 app.use(cors({
     origin: 'https://crud-example-d15v.onrender.com',
     methods: ['POST', 'GET', 'DELETE', 'PUT'],
     allowedHeaders:'*',
-    maxAge: 6400,
+    maxAge: 86400,
     credentials: true
 }))
 
 app.use(express.json())
 
 
-app.listen(5000, () => {
-    console.log('Aplicacao rodando na porta 5000')
+app.listen(8080, () => {
+    console.log('Aplicacao rodando na porta 8080')
 })
