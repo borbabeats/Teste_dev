98b5d55f9a56423a584b534c47427077e8ffd05e
Maicon Borba <68969007+borbabeats@users.noreply.github.com>
2024-02-19 08:53:20 -0300

    Update server.js
    new port using env

--- Backend/server.js
+++ Backend/server.js
@@ -10,19 +10,18 @@ app.use(cors({
     origin: 'https://teste-dev-pi.vercel.app',
     methods: ['POST', 'GET', 'DELETE', 'PUT'],
     allowedHeaders:['Content-Type', 'Authorization', 'Accept', 'Accept-Language', 'Content-Language'],
-    exposedHeaders:'*',
-    maxAge: 86400,
-    credentials: true
+    credentials: true //Allow credentials (cookies) to be sent cross-origin
 }))
 
 app.use(express.json())
 app.use(router)
 
-//app.options('*', cors())
+//Enable pre-flight requests
+app.options('*', cors())
+
+const PORT = process.env.PORT || 443
 
 
-
-
-app.listen(8080, () => {
-    console.log('Aplicacao rodando na porta 8080')
+app.listen(PORT, () => {
+    console.log('Aplicacao rodando na porta ${PORT}')
 })
