bd37690e054d6009d72d31a0e10e044339234aba
Maicon Borba <maiconbsconceicao@gmail.com>
2024-06-08 20:26:13 -0300

    Retornando, motivo: bloqueado pelo cors novamente

--- Backend/server.js
+++ Backend/server.js
@@ -5,17 +5,15 @@ const router = require('./src/routes/routes.js')
 const app = express()
 
 app.use(cors({
-    origin: '*',
+    origin: 'https://cadastros-client-side.onrender.com',
     methods: ['POST', 'GET', 'DELETE', 'PUT', 'OPTIONS'],
     allowedHeaders:['Content-Type', 'Authorization', 'Accept', 'Accept-Language', 'Content-Language', 'X-Requested-with'],
     maxAge: 86400,
     credentials: true
 }))
 
-
 app.use(express.json())
 app.use(router)
-
 
 app.listen(443, () => {
     console.log('Aplicacao rodando na porta 443')
